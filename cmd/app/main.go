918eb1205eac62910253401afbc97a98903de943
Dominik Tarnowski <dom@tdom.dev>
2020-12-15 03:02:40 +0000

    feat: connect to postgres

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/td0m/go-starter/pkg/util/env"
+)
 
 func main() {
-	http.ListenAndServe(":8080", nil)
+	_ = initPostgres(env.PostgresURI)
+
+	http.ListenAndServe(":"+env.Port, nil)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func initPostgres(uri string) (pg *sqlx.DB) {
+	i := 0
+	var err error
+	for i < 5 {
+		pg, err = sqlx.Connect("postgres", uri)
+		if err == nil {
+			log.Println("Postgres connected.")
+			return
+		}
+		i++
+		log.Println("Failed to connect to postgres. Trying again...")
+		time.Sleep(time.Second)
+	}
+	log.Panic(err)
+	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/td0m/go-starter
 
 go 1.15
+
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.0.0
+	google.golang.org/appengine v1.6.7 // indirect
+)
